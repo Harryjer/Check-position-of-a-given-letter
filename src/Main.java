aa6ffd09a55972538a95b73df28f02ba941dd8f7
Thisal Kodithuwakku <33801630+Harryjer@users.noreply.github.com>
2025-02-27 22:50:37 +0530

    add the comments on steps for better understanding

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,26 @@ public class Main {
     public static void main(String[] args) {
 //ASCII => a-97, A-65
 
+        //taking the letter from the user
         Scanner scanner = new Scanner(System.in);
         System.out.println("Provide an English Leteer");
 
+        //take the letters from the begining and save it on a variable
         char letter =scanner.next().charAt(0);
+
+        //if the user enters a capital letter, this convert the letter in to lower case
         letter =Character.toLowerCase(letter);
+
+        //store this letter value by converting it to an int.
         int ascivalue = (int)letter;
 
+        //print the asci value
         System.out.println(ascivalue);
+
+        //convert asci value for real time value
         int position = ascivalue-96;
+
+        //print the real time value.
         System.out.println("Position is : " + position);
 
     }
